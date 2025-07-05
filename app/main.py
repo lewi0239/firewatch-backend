5e307f8a55bc80e0a934454b2e02e5584ef34ac0
Brodie Lewis <lewi0239@algonquinlive.com>
2025-07-05 17:00:45 -0400

    finished CRUD

--- app/main.py
+++ app/main.py
@@ -18,12 +18,14 @@ app.add_middleware(
     allow_headers=["*"],  # Allows all headers
 )
 
+
 @app.get("/")
 def read_root():
     """
     Root endpoint for the API.
     """
     return {"message": "Welcome to the Firewatch API. Use /docs to see the documentation."}
+
 
 # Include routers for different features
 app.include_router(fire_report.router, prefix="/api", tags=["Fire Reports"])
